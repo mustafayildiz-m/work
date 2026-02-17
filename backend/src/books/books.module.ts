c66cb716b7bdeacd87b9714d0eaa2e304163dc8f
Mustafa <mustafayildiz.m@gmail.com>
2026-02-17 05:10:30 +0300

    12

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -7,14 +7,24 @@ import { Book } from './entities/book.entity';
 import { BookTranslation } from './entities/book-translation.entity';
 import { UploadModule } from '../upload/upload.module';
 import { BookCategory } from './entities/book-category.entity';
+import { BookPage } from './entities/book-page.entity';
+import { BookPageTranslation } from './entities/book-page-translation.entity';
+import { TranslationModule } from '../modules/translation.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Book, BookTranslation, BookCategory]),
+    TypeOrmModule.forFeature([
+      Book,
+      BookTranslation,
+      BookCategory,
+      BookPage,
+      BookPageTranslation,
+    ]),
     UploadModule,
+    TranslationModule,
   ],
   controllers: [BooksController, BookCategoriesController],
   providers: [BooksService],
   exports: [BooksService],
 })
-export class BooksModule {}
+export class BooksModule { }
