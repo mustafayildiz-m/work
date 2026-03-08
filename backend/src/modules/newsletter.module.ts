882b1e7849767075502f35890acf9a5f3584e578
Mustafa <mustafayildiz.m@gmail.com>
2026-03-09 02:37:26 +0300

    12

--- backend/src/modules/newsletter.module.ts
+++ backend/src/modules/newsletter.module.ts
@@ -4,10 +4,11 @@ import { Newsletter } from '../entities/newsletter.entity';
 import { NewsletterService } from '../services/newsletter.service';
 import { NewsletterController } from '../controllers/newsletter.controller';
 import { UploadModule } from '../upload/upload.module';
+import { CacheService } from '../services/cache.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Newsletter]), UploadModule],
-  providers: [NewsletterService],
+  providers: [NewsletterService, CacheService],
   controllers: [NewsletterController],
   exports: [NewsletterService],
 })
