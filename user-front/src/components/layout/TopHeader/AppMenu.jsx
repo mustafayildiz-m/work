d9523608417bc034e3375d6baa5628c07e2cdb23
Mustafa <mustafayildiz.m@gmail.com>
2026-03-09 17:01:08 +0300

    12

--- user-front/src/components/layout/TopHeader/AppMenu.jsx
+++ user-front/src/components/layout/TopHeader/AppMenu.jsx
@@ -7,6 +7,8 @@ import { Fragment, useCallback, useEffect, useState } from 'react';
 import { Dropdown, DropdownDivider, DropdownItem, DropdownMenu, DropdownToggle } from 'react-bootstrap';
 import { FaChevronDown, FaPlus } from 'react-icons/fa6';
 import { findAllParent, findMenuItem, getAppMenuItems, getMenuItemFromURL } from '@/helpers/menu';
+import { useLanguage } from '@/context/useLanguageContext';
+
 const MenuItemWithChildren = ({
   item,
   activeMenuItems,
@@ -14,22 +16,23 @@ const MenuItemWithChildren = ({
   linkClassName,
   level
 }) => {
+  const { t } = useLanguage();
   const level1 = level === 1;
   const Icon = item.icon;
   return <Dropdown as="li" className={itemClassName} drop={level >= 2 ? 'end' : undefined}>
-      <DropdownToggle as={Link} href="" role="button" className={clsx('content-none', linkClassName)} data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-haspopup="true" aria-expanded="false">
-        {Icon && <Icon className="text-success me-2" />}
-        {item.label && item.label}
-        {level1 ? <FaChevronDown size={8} className="ms-1" /> : <FaPlus size={10} />}
-      </DropdownToggle>
-      <DropdownMenu as="ul" aria-labelledby="listingMenu" className="custom-navbar-dropdown-menu" data-bs-popper="static" renderOnMount>
-        {(item.children ?? []).map((child, idx) => <Fragment key={idx + child.key + idx}>
-            {child.children ? <MenuItemWithChildren item={child} level={level + 1} activeMenuItems={activeMenuItems} itemClassName="dropdown-submenu" linkClassName={clsx('dropdown-item dropdown-toggle arrow-none d-flex align-items-center justify-content-between', {
+    <DropdownToggle as={Link} href="" role="button" className={clsx('content-none', linkClassName)} data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-haspopup="true" aria-expanded="false">
+      {Icon && <Icon className="text-success me-2" />}
+      {item.label && t(item.label)}
+      {level1 ? <FaChevronDown size={8} className="ms-1" /> : <FaPlus size={10} />}
+    </DropdownToggle>
+    <DropdownMenu as="ul" aria-labelledby="listingMenu" className="custom-navbar-dropdown-menu" data-bs-popper="static" renderOnMount>
+      {(item.children ?? []).map((child, idx) => <Fragment key={idx + child.key + idx}>
+        {child.children ? <MenuItemWithChildren item={child} level={level + 1} activeMenuItems={activeMenuItems} itemClassName="dropdown-submenu" linkClassName={clsx('dropdown-item dropdown-toggle arrow-none d-flex align-items-center justify-content-between', {
           active: activeMenuItems?.includes(child.key)
         })} /> : <MenuItem item={child} level={level + 1} linkClassName={clsx(activeMenuItems?.includes(child.key) && 'active')} />}
-          </Fragment>)}
-      </DropdownMenu>
-    </Dropdown>;
+      </Fragment>)}
+    </DropdownMenu>
+  </Dropdown>;
 };
 const MenuItem = ({
   item,
@@ -38,18 +41,19 @@ const MenuItem = ({
   itemClassName
 }) => {
   return item.isDivider ? <DropdownDivider /> : <li className={itemClassName}>
-      <MenuItemLink item={item} linkClassName={linkClassName} level={level + 1} />
-    </li>;
+    <MenuItemLink item={item} linkClassName={linkClassName} level={level + 1} />
+  </li>;
 };
 const MenuItemLink = ({
   item,
   linkClassName
 }) => {
+  const { t } = useLanguage();
   const Icon = item.icon;
   return <DropdownItem as={Link} href={item.url ?? ''} target={item.target} className={linkClassName}>
-      {Icon && <Icon className="text-success me-2" />}
-      {item.label && item.label}
-    </DropdownItem>;
+    {Icon && <Icon className="text-success me-2" />}
+    {item.label && t(item.label)}
+  </DropdownItem>;
 };
 const AppMenu = () => {
   const [activeMenuItems, setActiveMenuItems] = useState([]);
@@ -69,13 +73,13 @@ const AppMenu = () => {
     activeMenu();
   }, [activeMenu, menuItems]);
   return <ul className={clsx('navbar-nav navbar-nav-scroll ms-auto')}>
-      {(menuItems ?? []).map((item, idx) => {
+    {(menuItems ?? []).map((item, idx) => {
       return <Fragment key={item.key + idx}>
-            {item.children ? <MenuItemWithChildren item={item} activeMenuItems={activeMenuItems} level={1} itemClassName="nav-item" linkClassName={clsx('nav-link content-none d-flex align-items-center gap-1 justify-content-between', {
+        {item.children ? <MenuItemWithChildren item={item} activeMenuItems={activeMenuItems} level={1} itemClassName="nav-item" linkClassName={clsx('nav-link content-none d-flex align-items-center gap-1 justify-content-between', {
           active: activeMenuItems.includes(item.key)
         })} /> : <MenuItem item={item} level={1} itemClassName="nav-item" linkClassName={clsx('nav-link', activeMenuItems.includes(item.key) && 'active')} />}
-          </Fragment>;
+      </Fragment>;
     })}
-    </ul>;
+  </ul>;
 };
 export default AppMenu;
