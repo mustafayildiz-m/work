d9eaf07d8eb4b524571c3a640cf4c2cddaa012d6
Mustafa <mustafayildiz.m@gmail.com>
2026-03-10 10:35:01 +0300

    12

--- user-front/src/components/form/PasswordFormInput.jsx
+++ user-front/src/components/form/PasswordFormInput.jsx
@@ -21,14 +21,20 @@ const PasswordFormInput = ({
           {label && (typeof label === 'string' ? <FormLabel htmlFor={id ?? name} className={labelClass}>
                 {label}
               </FormLabel> : <>{label}</>)}
-          <div className="position-relative">
-            <FormControl id={id} type={showPassword ? 'text' : 'password'} {...other} {...field} isInvalid={Boolean(fieldState.error?.message)} />
+          <div>
+            <div className="position-relative d-inline-block w-100">
+              <FormControl id={id} type={showPassword ? 'text' : 'password'} {...other} {...field} isInvalid={Boolean(fieldState.error?.message)} />
+              <span
+                className="d-flex align-items-center justify-content-center position-absolute top-50 end-0 translate-middle-y p-0 pe-2"
+                style={{ zIndex: 10 }}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash size={18} className="cursor-pointer" /> : <FaEye size={18} className="cursor-pointer" />}
+              </span>
+            </div>
             {!noValidate && fieldState.error?.message && <Feedback type="invalid" className="text-start">
                 {fieldState.error?.message}
               </Feedback>}
-            <span className="d-flex  position-absolute top-50 end-0 translate-middle-y p-0 pe-2 me-2" onClick={() => setShowPassword(!showPassword)}>
-              {!fieldState.error && (showPassword ? <FaEyeSlash size={18} className="cursor-pointer" /> : <FaEye size={18} className="cursor-pointer" />)}
-            </span>
           </div>
         </FormGroup>} />;
 };
