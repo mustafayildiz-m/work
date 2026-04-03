0fa9e9b21a621bf3dd61be5feffef49c4ec44a2b
Mustafa <mustafayildiz.m@gmail.com>
2026-04-03 17:26:02 +0300

    Fix mailer EjsAdapter import for production docker image

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -1,5 +1,6 @@
 import { MailerModule } from '@nestjs-modules/mailer';
-import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+// @ts-ignore
+import { EjsAdapter } from '@nestjs-modules/mailer/adapters/ejs.adapter';
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
