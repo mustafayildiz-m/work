c25f1f4562aaa3c80626d8646cf4301ad2a37cb3
Mustafa <mustafayildiz.m@gmail.com>
2026-04-03 21:21:41 +0300

    fix: iframe csp politikasına youtube-nocookie eklendi

--- user-front/next.config.mjs
+++ user-front/next.config.mjs
@@ -21,6 +21,7 @@ const frameSrcOrigins = new Set([
   'https://www.youtube.com',
   'https://youtube.com',
   'https://www.youtube-nocookie.com',
+  'https://youtube-nocookie.com',
   'https://player.vimeo.com',
   'https://www.google.com',
 ]);
